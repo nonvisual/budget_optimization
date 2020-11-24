c61e3dc553752097de26b2258cc17ec21b7b472b
nonvisual <nonvisual.wrk@gmail.com>
2020-11-24 13:46:33 +0100

    Added data preparation notebook and preparation module

---
+++ preparation/data_preparer.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from preparation.rules_preparer import prepare_rules
+from preparation.types_classifier import assign_type
+from functools import partial
+
+
+def prepare_data(data: pd.DataFrame):
+    prepare_date_columns(data)
+    rules = prepare_rules()
+    f = partial(
+        assign_type,
+        types_mapping=rules,
+        info_columns=["Beneficiary / Originator", "Payment Details"],
+    )
+    data["type"] = data.apply(f, axis=1)
+
+
+def prepare_date_columns(data: pd.DataFrame):
+    data["Booking date"] = pd.to_datetime(data["Booking date"])
+    data["day"] = data["Booking date"].dt.day
+    data["month"] = data["Booking date"].dt.month
+    data["year"] = data["Booking date"].dt.year
+    data["weekday"] = data["Booking date"].dt.weekday
